3ed192e26f11f421fb9f147ad99a2bf14691e0d8
henry-cv <thechosenone.cc@gmail.com>
2023-12-03 20:24:21 -0500

    no ticket eliminar funciones

--- src/number-utils.js
+++ src/number-utils.js
@@ -10,12 +10,3 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
-function power(a, b) {
-  return a ** b;
-}
-function round(a) {
-  return Math.round(a);
-}
-function roundUp(a) {
-  return Math.ceil(a);
-}
